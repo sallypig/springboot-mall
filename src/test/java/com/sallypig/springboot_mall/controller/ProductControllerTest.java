0031f22d12b717c2c1c7235b76d96552e14f1ed1
2300553 <xiwenchu@systex.com>
2024-09-11 13:45:58 +0800

    商品功能 -單元測試

---
+++ src/test/java/com/sallypig/springboot_mall/controller/ProductControllerTest.java
@@ -0,0 +1,40 @@
+package com.sallypig.springboot_mall.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testCreateProduct() {
+
+    }
+
+    @Test
+    void testDeleteProduct() {
+
+    }
+
+    @Test
+    void testGetProduct() {
+
+    }
+
+    @Test
+    void testGetProducts() {
+
+    }
+
+    @Test
+    void testUpdateProduct() {
+
+    }
+}
